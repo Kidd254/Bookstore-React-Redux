52e3e25d4a16dbbbe62ae7aca4d854c4b6200e93
Kidd254 <lorenkioko@gmail.com>
2023-07-18 00:56:55 +0300

    add an initial state of booklist array and an add a remove reducer

---
+++ src/redux/books/booksSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { v4 as uuidv4 } from 'uuid';
+
+const initialState = {
+    bookList: [],
+}
+
+const booksSlice = createSlice({
+    name: 'books',
+    initialState,
+    add:(action, state)=>{
+        state.bookList.push({
+            title:action.payload.title,
+            author: action.payload.author,
+            id: uuidv4(),
+    })
+    },
+    remove(state, action){
+        return {
+            ...state,
+            booklist: state.booklist.filter((books)=> books.id!== action.payload.books.id)
+        }
+    }
+});
+export const {add, remove} = booksSlice.actions;
+export default booksSlice.reducer;
