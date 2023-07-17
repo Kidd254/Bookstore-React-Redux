ee8dcfeaa86aaa3849cd7fa3e5fe31497a1b7dc4
Kidd254 <lorenkioko@gmail.com>
2023-07-18 00:59:51 +0300

    import slice reducers into store

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import categoriesReducer from "./categories/categoriesSlice";
+import booksReducer from "./books/booksSlice";
+const store = configureStore({
+    reducer:{
+categories: categoriesReducer,
+books: booksReducer,
+
+    }
+})
+
+export default store;
