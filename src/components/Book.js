27788713e5e786f558a1751f685ac5834d3db627
Kidd254 <lorenkioko@gmail.com>
2023-07-18 18:42:01 +0300

    fix linters

--- src/components/Book.js
+++ src/components/Book.js
@@ -10,7 +10,7 @@ function Book({ bookData }) {
       <article className={styles.article}>
         <div className={styles.mainContent}>
           <h4 className={styles.bookCat}>Novel</h4>
-          <h2 className={styles.bookTitle}>{bookData.name}</h2>
+          <h2 className={styles.bookTitle}>{bookData.title}</h2>
           <h4 className={styles.bookCat}>{bookData.author}</h4>
           <ul className={styles.buttonList}>
             <li>Comments</li>
