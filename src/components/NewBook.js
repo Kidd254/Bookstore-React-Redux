ccda439a3eae2146fd46530a2d2475255933db78
Kidd254 <lorenkioko@gmail.com>
2023-07-18 15:24:16 +0300

    import useDispatch and develop functions for adding books

--- src/components/NewBook.js
+++ src/components/NewBook.js
@@ -1,17 +1,45 @@
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { add } from '../redux/books/booksSlice';
+
 function NewBook() {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const dispatch = useDispatch();
+
+  const handleTitle = (e) => {
+    e.preventDefault();
+    setTitle(e.target.value);
+  };
+
+  const handleAuthor = (e) => {
+    e.preventDefault();
+    setAuthor(e.target.value);
+  };
   return (
     <>
       <h4>ADD NEW BOOK</h4>
-      <form className="form-container">
+      <form
+        className="form-container"
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch(add({ title, author }));
+        }}
+      >
         <input
           type="text"
           placeholder="Book title"
           className="input-text"
+          value={title}
+          onChange={(e) => { handleTitle(e); }}
         />
         <input
           type="text"
           placeholder="Author name"
           className="input-text"
+          value={author}
+          onChange={(e) => { handleAuthor(e); }}
         />
         <button className="input-submit" type="submit">
           Add Book
