36c9afca5c461aaa4e3a68821f9dadb3632f6dc1
Kidd254 <lorenkioko@gmail.com>
2023-07-18 15:28:30 +0300

    import the initial state of books and assign it to bookList array

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,25 +1,28 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { v4 as uuidv4 } from 'uuid';
+import booksInitialState from '../../components/BooksLogic';
 
 const initialState = {
-  bookList: [],
+  bookList: booksInitialState,
 };
 
 const booksSlice = createSlice({
   name: 'books',
   initialState,
-  add: (action, state) => {
-    state.bookList.push({
-      title: action.payload.title,
-      author: action.payload.author,
-      id: uuidv4(),
-    });
-  },
-  remove(state, action) {
-    return {
-      ...state,
-      booklist: state.booklist.filter((books) => books.id !== action.payload.books.id),
-    };
+  reducers: {
+    add: (state, action) => {
+      state.bookList.push({
+        title: action.payload.title,
+        author: action.payload.author,
+        id: uuidv4(),
+      });
+    },
+    remove(state, action) {
+      return {
+        ...state,
+        bookList: state.bookList.filter((books) => books.id !== action.payload.books.id),
+      };
+    },
   },
 });
 export const { add, remove } = booksSlice.actions;
