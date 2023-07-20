824c8c2a12b846b07c227704a9c12cf8fd0e8e63
Kidd254 <lorenkioko@gmail.com>
2023-07-20 16:08:18 +0300

    add styling to category section

--- src/components/Classifications.js
+++ src/components/Classifications.js
@@ -1,18 +1,24 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categoriesSlice';
 import styles from '../Styles/Category.css';
 
 function Categories() {
+  const { status } = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
   return (
     <div className={styles.homeContainer}>
       <h2 className={styles.h2}>Welcome to our main page!</h2>
-      <button type="button">Check Status</button>
+      <button
+        className={styles.primaryBtnBig}
+        type="button"
+        onClick={() => {
+          dispatch(checkStatus());
+        }}
+      >
+        Check Status
+      </button>
       <p className={styles.p}>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-        Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,
-        when an unknown printer took a galley of type and scrambled it to make a type specimen book
-        It has survived not only five centuries, but also the leap into electronic typesetting,
-        remaining essentially unchanged. It was popularised in the 1960s with the release of
-        Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
-        software like Aldus PageMaker including versions of Lorem Ipsum.
+        {status}
       </p>
     </div>
   );
