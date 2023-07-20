05b7c65507572134830a51762657e282ca915bbd
Kidd254 <lorenkioko@gmail.com>
2023-07-20 15:58:58 +0300

    add classes and properties no nav bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,34 +1,46 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { BsFillPersonFill } from 'react-icons/bs';
 import { NavLink } from 'react-router-dom';
 import styles from '../Styles/Navigation.css';
 
 function Navbar() {
   return (
     <nav className={styles.nav}>
-      <h1 className={styles.h1}>Bookstore</h1>
-      <ul className={styles.ul}>
-        <li className={styles.li}>
-          <NavLink
-            to="/"
-            className={({ isActive }) => {
-              const active = isActive ? styles.active : styles.navLink;
-              return active;
-            }}
-          >
-            Books
-          </NavLink>
-        </li>
-        <li className={styles.li}>
-          <NavLink
-            to="categories"
-            className={({ isActive }) => {
-              const active = isActive ? styles.active : styles.navLink;
-              return active;
-            }}
-          >
-            Categories
-          </NavLink>
-        </li>
-      </ul>
+      <div className={styles.left}>
+        <h1 className={styles.h1}>Bookstore CMS</h1>
+        <ul className={styles.ul}>
+          <li className={styles.li}>
+            <NavLink
+              to="/"
+              className={({ isActive }) => {
+                const active = isActive ? styles.active : styles.navLink;
+                return active;
+              }}
+            >
+              Books
+            </NavLink>
+          </li>
+          <li className={styles.li}>
+            <NavLink
+              to="categories"
+              className={({ isActive }) => {
+                const active = isActive ? styles.active : styles.navLink;
+                return active;
+              }}
+            >
+              Categories
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.rigth}>
+        <div className={styles.iconBtn}>
+          <BsFillPersonFill
+            color="#0290ff"
+            size={22}
+          />
+        </div>
+      </div>
     </nav>
   );
 }
