edf42c5007f331eb06f5331a64b6076fabbeca2c
Kidd254 <lorenkioko@gmail.com>
2023-08-13 22:56:27 +0300

    add class to accomodate background image

--- src/components/Books.js
+++ src/components/Books.js
@@ -5,7 +5,7 @@ import styles from '../stylesF/Books.module.css';
 function Books() {
   return (
     <>
-      <div className={`container ${styles.background}`}>
+      <div className={`container-fluid ${styles.background}`}>
         <div className="d-flex flex-column justify-content-center align-items-center p-5">
           <BookList />
           <div className={styles.horizontalDivider}> </div>
